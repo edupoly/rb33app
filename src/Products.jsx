4b136bce0b0be6def1299869dcee963566c372b0
edupoly <info.edupoly@gmail.com>
2024-05-09 10:15:27 +0530

    mapping state dispatch

--- src/Products.jsx
+++ src/Products.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 function Products(props) {
+  console.log("Products rendered")
   return (
-    <div className='border border-2 border-warning m-2 p-2'>
+    <div className='border border-2 border-warning m-2 p-2' style={{height:"200px",overflow:'scroll'}}>
         <h1>Products</h1>
         <ul className='d-flex flex-wrap'>
             {
-                props.productsReducer.products.map((p)=>{
+                props.products.map((p)=>{
                     return <li className='w-25 p-5 d-flex  flex-column justify-content-between'>
                                 <p>{p.title.slice(0,20)}</p>
                                 <img src={p.image} className='w-100' alt="" />
                                 <h3>{p.price}</h3>
-                                <button className='btn btn-success' onClick={()=>{props.dispatch({type:'ADDTOCART',payload:p})}}> Add To Cart</button>
+                                <button className='btn btn-success' onClick={()=>{props.addToCart(p)}}> Add To Cart</button>
                         </li>
                 })
             }
@@ -21,4 +22,12 @@ function Products(props) {
   )
 }
 
-export default connect(store=>store)(Products)
+export default connect(function(state){
+  return state.productsReducer
+},function(dispatch){
+  return {
+    addToCart:(product)=>{
+      dispatch({type:"ADDTOCART",payload:product})
+    }
+  }
+})(Products)
